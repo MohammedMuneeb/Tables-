e5feaaf137b04c6ede64939807337379cef5a8c3
muneebmohammed.ethreyas@gmail.com <muneebmohammed.ethreyas@gmail.com>
2024-11-25 07:19:34 +0530

    first commit

---
+++ backend/config.js
@@ -0,0 +1,24 @@
+export const PORT =5174 ; 
+export const mongoDBURL ="mongodb+srv://mohammedmuneeb:oCjRhwGR3GhxUCqX@database.3g8mp.mongodb.net/?retryWrites=true&w=majority&appName=Database" 
+
+
+
+
+
+// ="mongodb+srv://mohammedmuneeb:Wxyzabcd@book.7yd9h.mongodb.net/books-collection?retryWrites=true&w=majority&appName=Book" 
+//="mongodb+srv://mohammedmuneeb:Wxyzabcd@test.oiiq4.mongodb.net/?retryWrites=true&w=majority&appName=Test"   
+
+
+
+// 'mongodb+srv://root:root@books-store-mern.c7ba8z1.mongodb.net/books-collection?retryWrites=true&w=majority';
+
+
+
+
+// Please create a free database for yourself.
+
+
+
+
+
+// This database will be deleted after tutorial
